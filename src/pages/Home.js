d9e18e99af527afb08b3cca16b61c1db8ce1a0ac
Alef Alexandre Silva Corrêa <102564597+Alef1412@users.noreply.github.com>
2023-12-07 19:23:59 -0300

    update routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import WordPressPosts from './widgets/WordPressPosts';
 import ProductList from './widgets/ProductList';
 import '../assets/css/home.css';
 
 function Home() {
   return (
-    <div className="Home">
-      <h1>Meu Aplicativo React com WordPress</h1>
-      <WordPressPosts />
+    <main className="Home">
+      <section class="banner_home row">
+
+      </section>
+      
       <ProductList />
-    </div>
+    </main>
   );
 }
 
--- src/pages/Midis.js
+++ src/pages/Midis.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import WordPressPosts from './widgets/WordPressPosts';
 
 function Midis() {
   return (
     <div className="Midis">
       <h1>Página de Midis</h1>
+      <WordPressPosts />
     </div>
   );
 }
