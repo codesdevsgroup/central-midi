9a738c0071ad6208571fc02b3acc7f8641844fca
Lucas Campos <bhlucascampos@gmail.com>
2023-12-15 11:19:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Substitua pelo caminho real do seu componente principal
+import App from './App';
 
-// Substitua o código existente com a nova API createRoot
-const root = ReactDOM.createRoot(document.getElementById('root')); // Substitua 'root' pelo ID do elemento onde você deseja renderizar o aplicativo
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />); // Substitua 'App' pelo seu componente principal
+root.render(<App />);
